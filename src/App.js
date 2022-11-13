823d955921678b27833b8ab74b27e5e3972fa7ac
Ankit Bansal <ankitbansal0002@gmail.com>
2022-11-13 17:38:54 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
 import Nav from './Nav';
+import HomeScreen from './HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="app">
-			<Nav />
-			<Banner />
-			<Row title="NETFLIX ORIGINALS" isLargeRow fetchUrl={requests.fetchNetflixOriginals} />
-			<Row title="Trending" fetchUrl={requests.fetchTrending} />
-			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+			<Router>
+          		<Routes>
+            		<Route path="/" element={<HomeScreen />}/>
+          		</Routes>
+      		</Router>
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyASl3-yDu6hkE153SgKbfP_-N5s5bNL6Xg",
+    authDomain: "netflix-2022-f43bf.firebaseapp.com",
+    projectId: "netflix-2022-f43bf",
+    storageBucket: "netflix-2022-f43bf.appspot.com",
+    messagingSenderId: "380660425178",
+    appId: "1:380660425178:web:d2db4cc607e4c81f8f20e3",
+    measurementId: "G-SWT74W1ENN"
+  };
