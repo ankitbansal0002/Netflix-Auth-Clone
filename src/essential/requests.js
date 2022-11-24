cb7905c8111a28679352e26c89bf92cc3320f65f
Ankit Bansal <ankitbansal0002@gmail.com>
2022-11-24 13:03:21 +0530

    Its a wrap for V_2.0

--- src/essential/requests.js
+++ src/essential/requests.js
@@ -8,7 +8,9 @@ const requests = {
 	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
 	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
 	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-	fetchDocumentaries: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+	fetchDocumentaries: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+	fetchAnimatedMovies: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+	fetchRegionalMovies: `discover/movie?api_key=${API_KEY}&with_watch_providers=1|2|9&watch_region=IN`
 };
 
 export default requests;
